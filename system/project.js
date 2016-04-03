4063ddac8cb7d25d3be87829eae7c53ae3233413
Gareth Fuller <gareth-fuller@hotmail.co.uk>
2016-04-04 00:09:31 +0200

    Start on the project generator
    silly silly node modules sooooo many!

--- system/project.js
+++ system/project.js
@@ -3,6 +3,8 @@
 var fs = require('fs');
 var log = require('../util/log');
 var Case = require('case');
+var replace = require('replace');
+require('shelljs/global');
 
 module.exports = function(name) {
   log(`creating project ${name}`);
@@ -32,5 +34,32 @@ module.exports = function(name) {
   fs.closeSync(fs.openSync('./app/components/.gitkeep', 'w'));
   fs.closeSync(fs.openSync('./app/interfaces/.gitkeep', 'w'));
 
-  log('cerated the files');
+  // Copy over the single files from the base into the new structure
+  fs.createReadStream(__dirname+'/base/.gitignore').pipe(fs.createWriteStream('./.gitignore'));
+  fs.createReadStream(__dirname+'/base/index.html').pipe(fs.createWriteStream('./index.html'));
+  fs.createReadStream(__dirname+'/base/package.json').pipe(fs.createWriteStream('./package.json'));
+  fs.createReadStream(__dirname+'/base/tsconfig.json').pipe(fs.createWriteStream('./tsconfig.json'));
+  fs.createReadStream(__dirname+'/base/typings.json').pipe(fs.createWriteStream('./typings.json'));
+  fs.createReadStream(__dirname+'/base/app/index.ts').pipe(fs.createWriteStream('./app/index.ts'));
+
+  // Copy the app component (recursive)
+  cp('-R', __dirname+'/base/app/components/app', './app/components/');
+  
+  // replace the constants
+  replace({
+    regex: 'APPNAME',
+    replacement: name,
+    paths: ['.'],
+    recursive: true,
+    exclude: './node_modules'
+  }, function(err, done) {
+    if (err) {
+      log(err, true);
+    } else {
+      installDeps();
+    }
+  });
+
+  log(`created the project ${name}`);
+
 };
