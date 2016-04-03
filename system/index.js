519282aa01b0e550bdb71e49824651b664a3a23e
Gareth Fuller <gareth-fuller@hotmail.co.uk>
2016-04-03 23:29:58 +0200

    Got the files being created

--- system/index.js
+++ system/index.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env node
+#!/usr/bin/env node --harmony
 
 // require all the functions
 var component = require('./component');
--- system/project.js
+++ system/project.js
@@ -1,5 +1,6 @@
-#!/usr/bin/env node
+#!/usr/bin/env node --harmony
 
+var fs = require('fs');
 var log = require('../util/log');
 var Case = require('case');
 
@@ -8,8 +9,28 @@ module.exports = function(name) {
 
   var kebab = Case.kebab(name);
 
-  console.log(kebab);
+  fs.mkdirSync(kebab);
+  process.chdir(kebab);
 
-  // make the dir and change into it
+  // Create and change into main folder
+  fs.mkdirSync('./app');
+  fs.mkdirSync('./app/components');
 
+  // create the folder structure
+  fs.mkdirSync('./app/lib');
+  fs.mkdirSync('./app/interfaces');
+  fs.mkdirSync('./app/mocks');
+  fs.mkdirSync('./app/pipes');
+  fs.mkdirSync('./app/services');
+  fs.mkdirSync('./app/styles');
+
+  // Create the git keep
+  fs.closeSync(fs.openSync('./app/lib/.gitkeep', 'w'));
+  fs.closeSync(fs.openSync('./app/mocks/.gitkeep', 'w'));
+  fs.closeSync(fs.openSync('./app/pipes/.gitkeep', 'w'));
+  fs.closeSync(fs.openSync('./app/services/.gitkeep', 'w'));
+  fs.closeSync(fs.openSync('./app/components/.gitkeep', 'w'));
+  fs.closeSync(fs.openSync('./app/interfaces/.gitkeep', 'w'));
+
+  log('cerated the files');
 };
